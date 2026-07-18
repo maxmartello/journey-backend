2713691909f337233950c6ec666504374f20f1e7
maxmartello <maxmartello@gmail.com>
2026-07-18 09:15:28 +0100

    Set up basic HTTP server in server.js

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
-console.log("Backend placeholder");
+import http from "http";
+
+const server = http.createServer((req, res) => {
+  res.end("Backend is running");
+});
+
+server.listen(process.env.PORT || 3000, () => {
+  console.log("Server started");
+});
